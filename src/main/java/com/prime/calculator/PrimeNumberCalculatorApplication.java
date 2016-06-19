72559ba00a38164a040ce45048c478cfac42d76a
Krzysztof Zielinski <krzysztoffzielinski@gmail.com>
2016-06-20 00:38:02 +0100

    initial files: pom and app

---
+++ src/main/java/com/prime/calculator/PrimeNumberCalculatorApplication.java
@@ -0,0 +1,12 @@
+package com.prime.calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeNumberCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeNumberCalculatorApplication.class, args);
+	}
+}
---
+++ src/test/java/com/prime/calculator/PrimeNumberCalculatorApplicationTests.java
@@ -0,0 +1,16 @@
+package com.prime.calculator;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = PrimeNumberCalculatorApplication.class)
+public class PrimeNumberCalculatorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
