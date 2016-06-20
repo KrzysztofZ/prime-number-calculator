f6e0917c661a5fa42f828e3a45c1c3bfd89551ef
Krzysztof Zielinski <krzysztoffzielinski@gmail.com>
2016-06-20 01:43:23 +0100

    implement naive algorithm

---
+++ src/main/java/com/prime/calculator/naive/NaivePrimeCalculatorService.java
@@ -0,0 +1,67 @@
+package com.prime.calculator.naive;
+
+import com.prime.calculator.IPrimeChecker;
+import com.prime.calculator.division.IPrimeCalculatorService;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CountDownLatch;
+import java.util.stream.IntStream;
+
+public class NaivePrimeCalculatorService implements IPrimeCalculatorService {
+
+    @Override
+    public List<Integer> calculate(int n) {
+        if(n<=0) {
+            throw new IllegalArgumentException("n needs to be positive.");
+        }
+        List<NaivePrimeChecker> chunkCheckers = initilize(n);
+
+        buildCheckers(n, chunkCheckers);
+        CountDownLatch latch = runComputations(chunkCheckers);
+        waitForCalculations(latch);
+
+        return convertResults(n);
+    }
+
+    private List<NaivePrimeChecker> initilize(int n) {
+        List<NaivePrimeChecker> chunkCheckers = new ArrayList<>();
+        NaivePrimeChecker.isPrime = new boolean[n +1];
+        IntStream.rangeClosed(0, n).forEach(i -> NaivePrimeChecker.isPrime[i] = true);
+        return chunkCheckers;
+    }
+
+    private void buildCheckers(int n, List<NaivePrimeChecker> chunkCheckers) {
+        int segmentSize = (int) n/5;
+        for(int start = 3; start <=n; start+=segmentSize + 1) {
+            int length = Math.min(segmentSize, n - start);
+            chunkCheckers.add(new NaivePrimeChecker(start, start + length));
+        }
+    }
+
+    private CountDownLatch runComputations(List<NaivePrimeChecker> chunkCheckers) {
+        CountDownLatch latch = new CountDownLatch(chunkCheckers.size());
+        for (IPrimeChecker primeChecker : chunkCheckers) {
+            CompletableFuture.runAsync(() -> primeChecker.check())
+                    .thenAccept(result -> {
+                        latch.countDown();
+                    });
+        }
+        return latch;
+    }
+
+    private void waitForCalculations(CountDownLatch latch) {
+        try {
+            latch.await();
+        } catch (InterruptedException e) {
+            throw new RuntimeException("Interrupted during calculations", e);
+        }
+    }
+
+    private List<Integer> convertResults(int n) {
+        List<Integer> result = new ArrayList<>();
+        IntStream.rangeClosed(2,n).filter(i -> NaivePrimeChecker.isPrime[i]).forEach( i -> result.add(Integer.valueOf(i)));
+        return result;
+    }
+}
